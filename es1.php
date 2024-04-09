74573a3228fe494d398eeffecfccb39ad6e315a4
MarquisPeroni <marco_p2009@live.it>
2024-04-09 17:01:47 +0200

    secondcommit

--- es1.php
+++ es1.php
@@ -49,3 +49,55 @@ foreach ($squadre_e_formazioni as $squadra => $formazione) {
     echo "<br>";
 }
 ?>
+
+<?php
+$partite_e_formazioni = array(
+    array(
+        "squadra_casa" => "Juventus",
+        "formazione_casa" => array("Portiere" => "Szczęsny", "Difensori" => array("De Ligt", "Chiellini", "Danilo", "Alex Sandro"), "Centrocampisti" => array("Bentancur", "Arthur", "Rabiot"), "Attaccanti" => array("Dybala", "Morata", "Kulusevski")),
+        "squadra_fuori_casa" => "Inter",
+        "formazione_fuori_casa" => array("Portiere" => "Handanović", "Difensori" => array("De Vrij", "Škriniar", "Bastoni"), "Centrocampisti" => array("Barella", "Brozović", "Vidal"), "Attaccanti" => array("Lautaro Martínez", "Perišić"))
+    ),
+    array(
+        "squadra_casa" => "Milan",
+        "formazione_casa" => array("Portiere" => "Donnarumma", "Difensori" => array("Hernández", "Tomori", "Kjær", "Calabria"), "Centrocampisti" => array("Kessié", "Bennacer"), "Attaccanti" => array("Rebić", "Ibrahimović", "Leão")),
+        "squadra_fuori_casa" => "Napoli",
+        "formazione_fuori_casa" => array("Portiere" => "Ospina", "Difensori" => array("Di Lorenzo", "Rrahmani", "Manolas", "Ghoulam"), "Centrocampisti" => array("Fabian Ruiz", "Demme", "Zielinski"), "Attaccanti" => array("Insigne", "Mertens"))
+    )
+);
+
+foreach ($partite_e_formazioni as $partita) {
+    if (isset($partita['squadra_casa']) && isset($partita['squadra_fuori_casa'])) {
+        echo "Partita: " . $partita['squadra_casa'] . " vs " . $partita['squadra_fuori_casa'] . "<br>";
+    }
+
+    if (isset($partita['formazione_casa'])) {
+        echo "Formazione " . $partita['squadra_casa'] . ":<br>";
+        foreach ($partita['formazione_casa'] as $ruolo => $giocatori) {
+            echo "$ruolo: ";
+            if (is_array($giocatori)) {
+                echo implode(", ", $giocatori);
+            } else {
+                echo $giocatori;
+            }
+            echo "<br>";
+        }
+        echo "<br>";
+    }
+
+    if (isset($partita['formazione_fuori_casa'])) {
+        echo "Formazione " . $partita['squadra_fuori_casa'] . ":<br>";
+        foreach ($partita['formazione_fuori_casa'] as $ruolo => $giocatori) {
+            echo "$ruolo: ";
+            if (is_array($giocatori)) {
+                echo implode(", ", $giocatori);
+            } else {
+                echo $giocatori;
+            }
+            echo "<br>";
+        }
+        echo "<br>";
+    }
+}
+?>
+
